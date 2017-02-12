77544c86286759d21922aa7baf4687d96ec006d8
Marbiru <code@uribram.com>
2017-02-12 09:57:05 +0200

    feature: input textbox and button, plus main.js, but not yet working

---
+++ platforms/browser/www/js/main.js
@@ -0,0 +1,60 @@
+var lazyDictionary = [
+"Alpha",
+"Bravo",
+"Charlie",
+"Delta",
+"Echo",
+"Foxtrot",
+"Golf",
+"Hotel",
+"India",
+"Juliett",
+"Kilo",
+"Lima",
+"Mike",
+"November",
+"Oscar",
+"Papa",
+"Quebec",
+"Romeo",
+"Sierra",
+"Tango",
+"Uniform",
+"Victor",
+"Whiskey",
+"X-Ray",
+"Yankee",
+"Zulu",
+"[NOT A LETTER]",
+];
+
+function translate_word() {
+  
+  var codewords = [];
+  
+  var input = document.getElementById("input");
+
+  var word = input.val();
+  
+  var codewords = [];
+  
+  for (var i = 0; i < word.length; i++) {
+      var letterIndex = word.charCodeAt(i);
+      console.log("input letter " + i + " is " +letterIndex);
+      if (letterIndex >= 65 && letterIndex <= 90)
+        letterIndex -= 65;
+      else if (letterIndex >= 97 && letterIndex <= 122)
+        letterIndex -= 97;
+      else
+        letterIndex = 26;
+      console.log("processed letter " + i + " is " +letterIndex);
+    codewords[i] = lazyDictionary[letterIndex];
+  };
+
+  return codewords.join("<br />");
+};
+
+function print_translation() {
+    var output = document.getElementById("output");
+    output.innerHTML = translate_word();
+};
